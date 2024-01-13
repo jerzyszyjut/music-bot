0d7061760e77bfb237356c6e56ae9a1cc8a2e31c
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2024-01-13 20:06:17 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 
 cd dev/music-bot/
 sudo docker pull $DOCKER_USER/$DOCKER_REPO:main
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 sudo docker run -d $DOCKER_USER/$DOCKER_REPO:main
