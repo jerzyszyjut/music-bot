a5b0e797e5a55b50cf87786cbd02d863928a17f3
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2024-01-13 19:38:59 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
 cd dev/music-bot/
-sudo docker login -u $DOCKER_USER -p $DOCKER_PASS
+echo $DOCKER_PASSWORD | sudo docker login --password-stdin --username $DOCKER_USER
 sudo docker pull $DOCKER_USER/$DOCKER_REPO:main
 sudo docker run -d $DOCKER_USER/$DOCKER_REPO:main
