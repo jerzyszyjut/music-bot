1f954bbbca997ac7ed5f70aa9a88315ce417cb5f
Jerzy Szyjut <jerzy.szyjut@outlook.com>
2024-01-13 20:08:58 +0100

    echo

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ cd dev/music-bot/
 sudo docker pull $DOCKER_USER/$DOCKER_REPO:main
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
+echo "sudo docker run -d $DOCKER_USER/$DOCKER_REPO:main"
 sudo docker run -d $DOCKER_USER/$DOCKER_REPO:main
